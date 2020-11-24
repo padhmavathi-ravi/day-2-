95ea9a9299cffea6961ef81cba6c93ea959c2c83
padhmavathi.R <74906446+padhmavathi-ravi@users.noreply.github.com>
2020-11-24 17:45:22 +0530

    Add files via upload

---
+++ day 2.py
@@ -0,0 +1,19 @@
+ex:1
+a=b=c=100
+division=a/10
+print(division)
+mul=b*50
+print(mul)
+add=c+60
+print(add)
+
+
+ex:2
+str="tharu"
+print(str.replace("a","g"))
+
+ex:3
+a=56
+b=67.9
+print("int to float:",float(a))
+print("int to float:",int(b))
